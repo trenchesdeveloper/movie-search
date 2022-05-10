edaa3dd8fba57d7f7126f5d62774644dca5d1f71
sam <51648328+SamDTech@users.noreply.github.com>
2022-05-10 16:50:00 +0100

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 // Routing
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Components
 
+import NotFound from "./pages/NotFound";
+import Home from "./pages";
 // Styles
 import { GlobalStyle } from "./GlobalStyle";
 
+
 const App: React.FC = () => (
   <Router>
-    <Routes></Routes>
+
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
     <GlobalStyle />
   </Router>
 );
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+//components
+
+const Home: React.FC = () => {
+
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Home;
