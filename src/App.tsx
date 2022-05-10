e7e9c5bf6b17c0c224cdcf5bc98444699f8cdd9b
sam <51648328+SamDTech@users.noreply.github.com>
2022-05-10 15:57:46 +0100

    set up the routing and global styles using styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// Routing
+import { BrowserRouter as Router, Routes } from "react-router-dom";
+// Components
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Styles
+import { GlobalStyle } from "./GlobalStyle";
+
+const App: React.FC = () => (
+  <Router>
+    <Routes></Routes>
+    <GlobalStyle />
+  </Router>
+);
 
 export default App;
